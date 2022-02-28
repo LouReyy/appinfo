bbe024234401cac6fcfb8ad1441efafe7a7b1400
Samba Ndiaye <samba.ndiaye@eleve.isep.fr>
2022-02-28 14:01:30 +0100

    a

--- auth/index.php
+++ auth/index.php
@@ -13,7 +13,7 @@
         <body>    
           
             <div id = test>
-                <img class = "logo"src="logo_infinite.png"></img>
+                <img class = "logo"src="logog"></img>
             </div>
 
             <div id = ecran>
