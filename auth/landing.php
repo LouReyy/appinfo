b337faea7c5a52cbbe60ad78c5b087e78ba74851
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-03-16 15:35:43 +0100

    edit profil

--- auth/landing.php
+++ auth/landing.php
@@ -19,17 +19,68 @@
     
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="edit.css" media="screen" type="text/css" />
+
 
     
   </head>
   <body>
-        <div class="container">
+
+  <div id = "container1">
+
+
+        <header>
+        <div id ="logoimg">
+            <a  href="/appinfo/homepage/homepage.html"><img src="/appinfo/auth/logo_infinite.png" alt="logo"></a>
+        </div>  
+                <nav>
+                    <ul class="nav__links">
+                        <li><a href="/appinfo/homepage/homepage.html">Accueil</a></li>
+                        <li><a href="#">Votre chantier</a></li>
+                        <li><a href="#">Forum</a></li>
+                        <li><a href="/appinfo/contact/Page_onglets.html">Contactez-nous</a></li>
+                    </ul>
+                </nav>
+             <a class="cta" href="/appinfo/auth/index.php">Connexion</a>
+        </header>
+
+        <div id = "title">
+             <titre1>Modifier votre profil</titre1>
+             <img class = "forme1"src="forme1.png"></img>
+            </div>
+
+
+
+ 
+
+        <div id="container">
+
         
             <div class="text-center">
+
+
                     <h1 class="p-5">Bonjour <?php echo $data['pseudo']; ?> !</h1>
-                    <hr />
-                    <a href="deconnexion.php" class="btn btn-danger btn-lg">Déconnexion</a>
+
+                  
+                    <h1>Pseudo : <input type="pseudo" name="" class="form-control" value= "<?php echo $data['pseudo']; ?>" required="required"></h1>
+
+                    <h1>Votre email : <input type="email" name="" class="form-control" value= "<?php echo $data['email']; ?>" required="required"></h1>
+
+                    <h1>Nouveau mot de passe : <input type="password" name="" class="form-control" value= "" required="required"></h1>
+
+                    <h1>Confirmation mot de passe : <input type="password" name="" class="form-control" value= "" required="required"></h1>
+
+                    <button href="deconnexion.php" class="btn btn-danger btn-lg">Déconnexion</button> 
+
+
             </div>
-        </div>   
+            <div id = pp> </div>
+            
+        </div> 
+         
+
+  </div>
   </body>
+
+  
 </html>
