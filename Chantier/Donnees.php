6ed8ad8f90b8f1f89bd511a40067404b2ebf8409
RohanKUMAR77 <99662520+RohanKUMAR77@users.noreply.github.com>
2022-05-02 19:24:04 +0200

    donnees

--- Chantier/Donnees.php
+++ Chantier/Donnees.php
@@ -1,8 +1,23 @@
 <?php
-$connexion=new mysqli('localhost','root','','appinfo');
-if ($connexion->connect_error){
-    die("probleme");
+//$connexion=new mysqli('localhost','root','','appinfo');
+//if ($connexion->connect_error){
+    //die("probleme");
+//}
+//echo ("connexion reussie");
+$conn=mysqli_connect('localhost','root','','appinfo');
+if (!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+
 }
-$requete='SELECT Valeur FROM `capteur_values` WHERE id_utilisateur=1 AND type="temp";'
+//La connexion fonctionne
+$result=mysqli_query($conn,'SELECT `Valeur` FROM `capteur_values` WHERE `id_utilisateur`=1 AND `type`="temp"');
+$values=mysqli_fetch_all($result, MYSQLI_ASSOC);
+print_r($values);
+
+
+
+
+
+
 
 ?>
