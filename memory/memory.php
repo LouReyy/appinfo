5a1f7fb687fea983dd4ebacd37fe139220551313
Martyna0 <95086387+Martyna0@users.noreply.github.com>
2022-05-13 09:54:14 +0200

    memory js v1

--- memory/memory.php
+++ memory/memory.php
@@ -92,30 +92,7 @@ if(isset($_SESSION['user'])){
            </tr>
     </table>
     
-    <script>
-        var motifsCartes=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
-        var etatsCartes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; 
-        var cartesRetournees=[];
-        var nbPairesTrouvees=0;
-        var imgCartes=document.getElementById("jeu").getElementsByTagName("img");
-        
-        function majAffichage(){ /*Mise a jour de l'affichage du jeu*/
-	            
-	    }
-
-
-        function Verification(){ /*Verification des cartes, des paires etc */
-
-        }
-        
-        function Initialisation(){ /* Initialise le jeu */
-
-        }
-
-        function Rejouer(){ /*Boutton pour rejouer au jeu */
-
-        }
-
+    <script src="js/memory.js">
     </script>
 
     <h3> Nombre de coups:</h3>
