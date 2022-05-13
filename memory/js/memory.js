ce3b14a3f83dcf4aaeeb90505fc833fc83683527
Martyna0 <95086387+Martyna0@users.noreply.github.com>
2022-05-13 09:58:08 +0200

    memory v1.1

---
+++ memory/js/memory.js
@@ -0,0 +1,23 @@
+var motifsCartes=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+        var etatsCartes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; 
+        var cartesRetournees=[];
+        var nbPairesTrouvees=0;
+        var imgCartes=document.getElementById("jeu").getElementsByTagName("img");
+        
+        
+        function majAffichage(){ /*Mise a jour de l'affichage du jeu*/
+	            
+	    }
+
+
+        function Verification(){ /*Verification des cartes, des paires etc */
+
+        }
+        
+        function Initialisation(){ /* Initialise le jeu */
+
+        }
+
+        function Rejouer(){ /*Boutton pour rejouer au jeu */
+salut
+        }
