97eed315d95b611639eef24a57f10e126738a8e0
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-17 20:57:08 +0200

    Revert "modif mvc"
    This reverts commit 5a6765af371254e87358d636d364b5295de00403.

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -1,6 +1,21 @@
 <?php
 
-include("model/setup_homepage.php")
+session_start(); 
+
+if(isset($_SESSION['user'])){
+        $editprofil ="views/landing.php";
+        $title = "Profil";
+    }
+    else{
+        $editprofil ="index.php";
+        $title = "Connexion";
+
+    }
+    if(isset($_SESSION['type'])){
+        $chantier = "Chantier/PageChantier.php";    }
+    else{
+        $chantier = "VotreChantier/votrechantier.php";
+    }
 
 
 ?>
@@ -12,21 +27,149 @@ include("model/setup_homepage.php")
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/homepage.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="homepage.css" media="screen" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <title>Tech4Health</title>
 </head>
 <body>
-<?php include("views/header.php");?>
+<header>
+            <div id ="logoimg">
+            <a  href="/appinfo/homepage/homepage.php"><img src="/appinfo/auth/img/logo_infinite.png" alt="logo"></a>
+            </div>  
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="/appinfo/homepage/homepage.php">Accueil</a></li>
+                    <li><a href="/appinfo/<?php echo $chantier ?>" >Votre chantier</a></li>
+                    <li><a href="/appinfo/forum/forum.php">Forum</a></li>
+                    <li><a href="/appinfo/faq/faq.php">FAQ</a></li>
+                    <li><a href="/appinfo/contact/contact_essai.php">Contactez-nous</a></li>
+                    <li><a href="/appinfo/notre_solution/notre_solution.php">Notre solution</a></li>
 
-        
+                </ul>
+            </nav>
+            <div id="logomemo">
+                <a href="/appinfo/memory/memory.php"><img src="../memory/memoryim.png" alt="memory"></a>
+            </div>
+            <a class="cta" href= "/appinfo/auth/<?php  echo $editprofil?> "> <?php echo $title ?></a>
+
+            <?php
+           
+
+           if(isset($_SESSION['type']) && ($_SESSION['type']) == "Administrateur"){
+            
+            ?>
+            <a class="cta" href= "/appinfo/admin/admin.php">Admin</a>
+
+            <?php }?>
+        </header>
+
+        <main>
+		
+            <h1> Bienvenue 
+            <hr/>
+            </h1>
+            <br>
+            <div id="container">
+                <div id="enfantimage">
+                    <img src="eolienne1.png">
+                </div>
+                <div id="enfanttexte">
+                    <div class="boxdetexte">
+                    <h2><img src="deco1.png" align="left">L'environnement</h2>
+                    <p>Soucieux de l'environnement et du développement humain nous avons pensé une solution afin d'agir pour la cause des ouvriers. Les journées de travail sont donc souvent très longues ; souvent plus de 12 heures quotidiennes, quelquefois 15 heures. Ce rythme effréné couplé à de mauvaises conditions de travail peut être fatal pour un homme en développement  </p>
+                    </div>
+                </div>
+    </div>
+    <div id="container">
+                <div id="enfanttexte">
+                    <div class="boxdetexte">
+                    <h2>
+                    <img src="deco2.png" align="left">
+                    Les ouvriers ? </h2><br>
+                    <p>Bienvenue sur notre site Internet, que vous soyez architecte, maitre d'ouvre ou encore ouvrier alors ce site est fait pour vous ! En effet, notre projet est d'analyser l'impact sur la santé d'un chantier et de ses dérives. Le constat est sans appel, les conditions de travail sont en désaccord avec les droits humains à l'image des dérives au Qatar avec la création des stades. Inscrivez -vous sur notre site Internet afin d'avoir un aperçu des statistiques de votre chantier.</p>
+                    </div>
+                </div>
+                <div id="enfantimage">
+                    <img src="chantier1.png">
+                </div>
+            </div>
+
+    <h1>
+        Nos services
+        <hr />
+    </h1>
+<br>
+    <div id="containerpartie2">
+
+        <div class="chantier">
+            <img src="grues.jpg" alt="">
+            <h2>Votre chantier</h2>
+            <p>Accéder à cette section pour découvrir les informations sur votre chantier : Localisation, les dates & la nature du chantier.</p>
+            <a class = btn_forum href = "/appinfo/Chantier/PageChantier.php">Accès au chantier</a>
+
+        </div>
+
+
+        <div class="stats">
+            <img src="stats2.jpg" alt="">
+            <h2>Statistiques</h2>
+            <p>Accéder à cette page afin de constater les statistiqes de votre chantier, l'environnement de travail des ouvriers est notre priorité.</p>
+            <a class = btn_forum href = "/appinfo/Chantier/PageChantier.php">Accès aux statistiques</a>
+
+        </div>
+    
+
+        <div class="forum" >
+            <img src="forum.jpg" alt="">
+
+            <h2>Forum</h2>
+            <p>Sur cette page, vous trouverez un ensemble de questions-réponses pour différents sujets.</p>
+            <a class = btn_forum href = "/appinfo/forum/forum.php">Accès au forum</a>
+
+        </div>
+    </div>
+    </main> 
 </body>
 
-<?php include("views/main.php");
-include ("views/footer.php");?>
-
-
-
-
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class=" footer-col">
+                <img src="infinite.png" class="logo">
+                </div>
+            <div class=" footer-col">
+                <h4>NAVIGATION</h4>
+                <ul>
+                    <li><a href= "/appinfo/homepage/homepage.php">Accueil</a></li>
+                    <li><a href= "/appinfo/<?php echo $chantier ?>">Votre chantier</a></li>
+                    <li><a href= "/appinfo/forum/forum.php">Forum</a></li>
+                    <li><a href= "/appinfo/faq/faq.php">FAQ</a></li>
+                    <li><a href= "/appinfo/contact/contact_essai.php">Contactez-nous</a></li>
+                    <li><a href= "/appinfo/notre_solution/notre_solution.php">Notre solution</a></li>
+                </ul>
+            </div>
+            <div class=" footer-col">
+                <h4>PLUS D'INFOS</h4>
+                <ul>
+                    <li><a href= "/appinfo/auth/views/inscription.php">Inscription</a></li>
+                    <li><a href= "/appinfo/auth/model/connexion.php">Connexion</a></li>
+                    <li><a href= "/appinfo/cgu/cgu.php">Mentions Légales</a></li>
+                </ul>
+            </div>
+            <div class=" footer-col">
+                <h4>SUIVEZ-NOUS</h4>
+                <div class="social-links">
+                    <a href= "#"><i class="fab fa-facebook-f"></i></a>
+                    <a href= "#"><i class="fab fa-twitter"></i></a>
+                    <a href= "#"><i class="fab fa-instagram"></i></a>
+                    <a href= "#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
+</footer>
 
 
 </html>
--- memory/memory.php
+++ memory/memory.php
@@ -1,5 +1,16 @@
 <?php
-include("model/setup_memory.php")
+
+session_start(); 
+
+if(isset($_SESSION['user'])){
+    $editprofil ="views/landing.php";
+    $title = "Profil";
+    }
+    else{
+        $editprofil ="index.php";
+        $title = "Connexion";
+
+    }
 ?>
 
 
@@ -9,16 +20,42 @@ include("model/setup_memory.php")
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/memo.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="memo.css" media="screen" type="text/css" />
     <title>Memory</title>
 </head>
 <body>
 
 <div id = "container1">
+<header>
+            <div id ="logoimg">
+            <a  href="/appinfo/homepage/homepage.php"><img src="/appinfo/auth/img/logo_infinite.png" alt="logo"></a>
+            </div>  
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="/appinfo/homepage/homepage.php">Accueil</a></li>
+                    <li><a href="/appinfo/<?php echo $chantier ?>" >Votre chantier</a></li>
+                    <li><a href="/appinfo/forum/forum.php">Forum</a></li>
+                    <li><a href="/appinfo/faq/faq.php">FAQ</a></li>
+                    <li><a href="/appinfo/contact/contact_essai.php">Contactez-nous</a></li>
+                    <li><a href="/appinfo/notre_solution/notre_solution.php">Notre solution</a></li>
 
+                </ul>
+            </nav>
+            <div id="logomemo">
+                <a href="/appinfo/memory/memory.php"><img src="../memory/memoryim.png" alt="memory"></a>
+            </div>
+            <a class="cta" href= "/appinfo/auth/<?php  echo $editprofil?> "> <?php echo $title ?></a>
 
-<?php include("views/header.php") ?>
+            <?php
+           
 
+           if(isset($_SESSION['type']) && ($_SESSION['type']) == "Administrateur"){
+            
+            ?>
+            <a class="cta" href= "/appinfo/admin/admin.php">Admin</a>
+
+            <?php }?>
+        </header>
 
 <main>
 <h1> Memory <hr/></h1>
@@ -29,32 +66,32 @@ include("model/setup_memory.php")
 <div id="jeu2">
     <table id="tabledejeu2">
            <tr>
-            <td><img src="img/face.png" class = "img2" id= "1">
-            <td><img src="img/face.png" class = "img2" id= "2">
-            <td><img src="img/face.png" class = img2 id= "3">
-            <td><img src="img/face.png" class = img2 id= "4">
-            <td><img src="img/face.png" class = img2 id= "5">
+            <td><img src="face.png" class = "img2" id= "1">
+            <td><img src="face.png" class = "img2" id= "2">
+            <td><img src="face.png" class = img2 id= "3">
+            <td><img src="face.png" class = img2 id= "4">
+            <td><img src="face.png" class = img2 id= "5">
            </tr>
            <tr>
-            <td><img src="img/face.png" class = img2 id= "6">
-            <td><img src="img/face.png" class = img2 id= "7">
-            <td><img src="img/face.png" class = img2 id= "8">
-            <td><img src="img/face.png" class = img2 id= "9">
-            <td><img src="img/face.png" class = img2 id= "10">
+            <td><img src="face.png" class = img2 id= "6">
+            <td><img src="face.png" class = img2 id= "7">
+            <td><img src="face.png" class = img2 id= "8">
+            <td><img src="face.png" class = img2 id= "9">
+            <td><img src="face.png" class = img2 id= "10">
            </tr>
            <tr>
-            <td><img src="img/face.png" class = img2 id= "11">
-            <td><img src="img/face.png" class = img2 id= "12">
-            <td><img src="img/face.png" class = img2 id= "13">
-            <td><img src="img/face.png" class = img2 id= "14">
-            <td><img src="img/face.png" class = img2 id= "15">
+            <td><img src="face.png" class = img2 id= "11">
+            <td><img src="face.png" class = img2 id= "12">
+            <td><img src="face.png" class = img2 id= "13">
+            <td><img src="face.png" class = img2 id= "14">
+            <td><img src="face.png" class = img2 id= "15">
            </tr>
            <tr>
-            <td><img src="img/face.png" class = img2 id= "16">
-            <td><img src="img/face.png" class = img2 id= "17">
-            <td><img src="img/face.png" class = img2 id= "18">
-            <td><img src="img/face.png" class = img2 id= "19">
-            <td><img src="img/face.png" class = img2 id= "20">
+            <td><img src="face.png" class = img2 id= "16">
+            <td><img src="face.png" class = img2 id= "17">
+            <td><img src="face.png" class = img2 id= "18">
+            <td><img src="face.png" class = img2 id= "19">
+            <td><img src="face.png" class = img2 id= "20">
            </tr>
     </table>
 </div>
@@ -62,32 +99,32 @@ include("model/setup_memory.php")
 <div id="jeu" >
     <table id="tabledejeu">
            <tr>
-            <td><img src="img/face.png" class = "img1" id= "1a">
-            <td><img src="img/face.png" class = "img1" id= "2a">
-            <td><img src="img/face.png" class = img1 id= "3a">
-            <td><img src="img/face.png" class = img1 id= "4a">
-            <td><img src="img/face.png" class = img1 id= "5a">
+            <td><img src="face.png" class = "img1" id= "1a">
+            <td><img src="face.png" class = "img1" id= "2a">
+            <td><img src="face.png" class = img1 id= "3a">
+            <td><img src="face.png" class = img1 id= "4a">
+            <td><img src="face.png" class = img1 id= "5a">
            </tr>
            <tr>
-            <td><img src="img/face.png" class = img1 id= "6a">
-            <td><img src="img/face.png" class = img1 id= "7a">
-            <td><img src="img/face.png" class = img1 id= "8a">
-            <td><img src="img/face.png" class = img1 id= "9a">
-            <td><img src="img/face.png" class = img1 id= "10a">
+            <td><img src="face.png" class = img1 id= "6a">
+            <td><img src="face.png" class = img1 id= "7a">
+            <td><img src="face.png" class = img1 id= "8a">
+            <td><img src="face.png" class = img1 id= "9a">
+            <td><img src="face.png" class = img1 id= "10a">
            </tr>
            <tr>
-            <td><img src="img/face.png" class = img1 id= "11a">
-            <td><img src="img/face.png" class = img1 id= "12a">
-            <td><img src="img/face.png" class = img1 id= "13a">
-            <td><img src="img/face.png" class = img1 id= "14a">
-            <td><img src="img/face.png" class = img1 id= "15a">
+            <td><img src="face.png" class = img1 id= "11a">
+            <td><img src="face.png" class = img1 id= "12a">
+            <td><img src="face.png" class = img1 id= "13a">
+            <td><img src="face.png" class = img1 id= "14a">
+            <td><img src="face.png" class = img1 id= "15a">
            </tr>
            <tr>
-            <td><img src="img/face.png" class = img1 id= "16a">
-            <td><img src="img/face.png" class = img1 id= "17a">
-            <td><img src="img/face.png" class = img1 id= "18a">
-            <td><img src="img/face.png" class = img1 id= "19a">
-            <td><img src="img/face.png" class = img1 id= "20a">
+            <td><img src="face.png" class = img1 id= "16a">
+            <td><img src="face.png" class = img1 id= "17a">
+            <td><img src="face.png" class = img1 id= "18a">
+            <td><img src="face.png" class = img1 id= "19a">
+            <td><img src="face.png" class = img1 id= "20a">
            </tr>
     </table>
 
