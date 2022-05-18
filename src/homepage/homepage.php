19e93e12c191236a298be47bf8a95b1cf4fe17df
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-18 12:29:51 +0200

    Revert "test homepage"
    This reverts commit c031d508dc0b19a00ab38c14c6135e8b49d23125.

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -1,6 +1,21 @@
 <?php
 
-include("setup_homepage.php");
+session_start(); 
+
+if(isset($_SESSION['user'])){
+        $editprofil ="views/landing.php";
+        $title = "Profil";
+    }
+    else{
+        $editprofil ="index.php";
+        $title = "Connexion";
+
+    }
+    if(isset($_SESSION['type'])){
+        $chantier = "Chantier/PageChantier.php";    }
+    else{
+        $chantier = "VotreChantier/votrechantier.php";
+    }
 
 
 ?>
@@ -18,11 +33,11 @@ include("setup_homepage.php");
 <body>
 <header>
             <div id ="logoimg">
-            <a  href="/homepage/homepage.php"><img src="/auth/img/logo_infinite.png" alt="logo"></a>
+            <a  href="/appinfo/homepage/homepage.php"><img src="/appinfo/auth/img/logo_infinite.png" alt="logo"></a>
             </div>  
             <nav>
                 <ul class="nav__links">
-                    <li><a href="/homepage/homepage.php">Accueil</a></li>
+                    <li><a href="/appinfo/homepage/homepage.php">Accueil</a></li>
                     <li><a href="/appinfo/<?php echo $chantier ?>" >Votre chantier</a></li>
                     <li><a href="/appinfo/forum/forum.php">Forum</a></li>
                     <li><a href="/appinfo/faq/faq.php">FAQ</a></li>
