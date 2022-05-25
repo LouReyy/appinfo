f06976fb747ae1d1353120c525c92259eb0d0348
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-25 17:01:21 +0200

    push

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -10,6 +10,8 @@ include("model/setup_homepage.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/homepage.css" media="screen" type="text/css" />
+    <link rel="icon" type="image/png" href="img/InfiniteMeasures.png" />
+
     <title>Tech4Health</title>
 </head>
 <body>
