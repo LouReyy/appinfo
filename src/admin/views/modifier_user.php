48e32c62801c03376252ae959ea81564de1b999b
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-25 20:36:42 +0200

    push modif user pp

--- src/admin/views/modifier_user.php
+++ src/admin/views/modifier_user.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/modifier_user.css" media="screen" type="text/css" />
-    <title>Document</title>
+    <title>Modification Info Utilisateur</title>
 </head>
 <body>
 
