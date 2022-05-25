93936ba0b77b53e8c909f7c591850f211bc01ccc
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-25 20:12:23 +0200

    push

--- src/auth/views/edit_password.php
+++ src/auth/views/edit_password.php
@@ -27,7 +27,7 @@ else{
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/edit_password.css" media="screen" type="text/css" />
-    <title>Document</title>
+    <title>Modification de Mot de Passe</title>
 </head>
 <body>
     
