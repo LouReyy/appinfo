db8ec2c899028c1274249307e45612c456489e1b
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-25 17:19:55 +0200

    push footer homepage

--- src/forum/views/footer.php
+++ src/forum/views/footer.php
@@ -2,25 +2,25 @@
     <div class="container">
         <div class="row">
             <div class=" footer-col">
-                <img src="img/InfintesMesures.png" class="logo">
+                <img src="/auth/img/logo_infinite.png" class="logo">
                 </div>
             <div class=" footer-col">
                 <h4>NAVIGATION</h4>
                 <ul>
-                    <li><a href= "/appinfo/homepage/homepage.php">Accueil</a></li>
-                    <li><a href= "/appinfo/<?php echo $chantier ?>">Votre chantier</a></li>
-                    <li><a href= "/appinfo/forum/forum.php">Forum</a></li>
-                    <li><a href= "/appinfo/faq/faq.php">FAQ</a></li>
-                    <li><a href= "/appinfo/contact/contact_essai.php">Contactez-nous</a></li>
-                    <li><a href= "/appinfo/notre_solution/notre_solution.php">Notre solution</a></li>
+                    <li><a href= "/homepage/homepage.php">Accueil</a></li>
+                    <li><a href= "/<?php echo $chantier ?>">Votre chantier</a></li>
+                    <li><a href= "/forum/forum.php">Forum</a></li>
+                    <li><a href= "/faq/faq.php">FAQ</a></li>
+                    <li><a href= "/contact/contact_essai.php">Contactez-nous</a></li>
+                    <li><a href= "/notre_solution/notre_solution.php">Notre solution</a></li>
                 </ul>
             </div>
             <div class=" footer-col">
                 <h4>PLUS D'INFOS</h4>
                 <ul>
-                    <li><a href= "/appinfo/auth/views/inscription.php">Inscription</a></li>
-                    <li><a href= "/appinfo/auth/model/connexion.php">Connexion</a></li>
-                    <li><a href= "/appinfo/cgu/cgu.php">Mentions Légales</a></li>
+                    <li><a href= "/auth/views/inscription.php">Inscription</a></li>
+                    <li><a href= "/auth/model/connexion.php">Connexion</a></li>
+                    <li><a href= "/cgu/cgu.php">Mentions Légales</a></li>
                 </ul>
             </div>
             <div class=" footer-col">
