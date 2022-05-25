7eb82d21c591f63e48937eee5510f71b20f231c3
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-25 16:25:21 +0200

    push

--- src/Chantier/model/PageGestionnaire.php
+++ src/Chantier/model/PageGestionnaire.php
@@ -72,7 +72,7 @@ $size=count($allId);
                         $id=$allId[$i];
                         $pseudo=$allPesudo[$i];
                         if ($i==0){
-                            echo "<div class='local'><a href='GestionGestionnaire.php?id=$id&pseudo=$pseudo'>$pseudo</a></div>";  
+                            echo "<div class='local'href='GestionGestionnaire.php?id=$id&pseudo=$pseudo'>$pseudo</></div>";  
                         }
                         else{
                             echo "<div class='local other'><a href='GestionGestionnaire.php?id=$id&pseudo=$pseudo'>$pseudo</a></div>";
