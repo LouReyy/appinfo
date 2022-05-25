07129ef30fea8821c47f5a0bb959a488b726bd14
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-25 22:36:31 +0200

    push

--- src/admin/model/setup_modif.php
+++ src/admin/model/setup_modif.php
@@ -42,7 +42,7 @@ echo($data['email']);
 
 
 
-if(file_exists('https://appinfofinal.herogu.garageisep.com/auth/index.php')){
+if(file_exists('https://appinfofinal.herogu.garageisep.com/auth/profil_picture')){
     
     $file_name = "/auth/profil_picture/" . hash('sha256',  $data['email'] );
     }
