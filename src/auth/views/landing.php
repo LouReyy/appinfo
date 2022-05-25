7efe4b59211529333d4cd67614971590111d102a
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-25 16:21:51 +0200

    push

--- src/auth/views/landing.php
+++ src/auth/views/landing.php
@@ -73,7 +73,7 @@
                 </ul>
             </nav>
             <div id="logomemo">
-                <a href="/memory/memory.php"><img src="/memory/memoryim.png" alt="memory"></a>
+                <a href="/memory/memory.php"><img src="/memory/img/memoryim.png" alt="memory"></a>
             </div>
             <a class="cta" href= "/auth/<?php echo $editprofil?> "> <?php echo $title ?></a>
 
@@ -113,7 +113,7 @@
             <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off">
             </div>
             <div class="form-group">
-                <t>Numéro Chantier </t><input type="id_chantier" name="id_chantier" class="form-control" placeholder="Numero de chantier" required="required" autocomplete="off">
+                <t>Numéro Chantier </t><input type="id_chantier" name="id_chantier" class="form-control" placeholder="Numero de chantier" required="required" value = "<?php echo $data['id_chantier']; ?>"autocomplete="off">
             </div>
 
             <div class="form-group">
