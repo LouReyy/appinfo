2e1e47a6239fa1aa506044bedd97ddecfb70f4e2
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-28 13:46:23 +0200

    push

--- src/Chantier/model/ajout_chantier.php
+++ src/Chantier/model/ajout_chantier.php
@@ -8,7 +8,7 @@ require_once 'config.php';
     $id_chantier = htmlspecialchars($_POST['id_chantier']);
     $nom = htmlspecialchars($_POST['nom']);
     $localisation = htmlspecialchars($_POST['localisation']);
-    $date_debut = htmlspecialchars($_POST['$date_debut']);
+    $date_debut = htmlspecialchars($_POST['date_debut']);
     $date_fin = htmlspecialchars($_POST['date_fin']);
 
     echo($id_chantier . $nom . $localisation . $date_debut . $date_fin);
