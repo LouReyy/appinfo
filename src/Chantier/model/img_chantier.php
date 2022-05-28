23d4357ad85b65b557dc63b367f1acc650a2bb6d
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-28 18:28:49 +0200

    push

--- src/Chantier/model/img_chantier.php
+++ src/Chantier/model/img_chantier.php
@@ -26,7 +26,7 @@ if(!isset($_SESSION['user'])){
 
 
     if (isset($_FILES['picture']['tmp_name']) and strlen($_FILES['picture']['tmp_name'])) {
-         $retour = copy( $_FILES['picture']['tmp_name'] , '../img_chantier/test1.jpg');
+         $retour = copy( $_FILES['picture']['tmp_name'] , '../img_chantier');
         if($retour) {
             echo '<p>La photo a bien été envoyée.</p>';
             echo '<img src="' . $_FILES['picture']['name'] . '">';
