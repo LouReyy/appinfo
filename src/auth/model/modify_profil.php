25b9a4d23a3bbada6155cb9d8cd04f662dd96e53
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-28 12:40:09 +0200

    stp marche

--- src/auth/model/modify_profil.php
+++ src/auth/model/modify_profil.php
@@ -58,7 +58,7 @@ if($password === $password_retype){
 
 
     $update = $bdd->prepare('UPDATE utilisateurs SET pseudo = ?,password = ?, type = ?,id_chantier = ? WHERE email = ?');
-    $update->execute(array($pseudo,$password,$type,$email,$id_chantier));
+    $update->execute(array($pseudo,$password,$type,$id_chantier,$emai));
 
     if($admin == "true"){
         if (smtpmailer($to_email,$from_email,$name, $subject, $body, )) {
