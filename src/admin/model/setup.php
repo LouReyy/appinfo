d10e7cf1fce31440d31511d97df2efef45640e7f
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-28 20:30:47 +0200

    sc

--- src/admin/model/setup.php
+++ src/admin/model/setup.php
@@ -6,7 +6,7 @@ require_once 'config.php';
 
 if(!isset($_SESSION['type']) && ($_SESSION['type']) != "Administrateur"){
 
-header('Location: https://appfinal.herogu.garageisep.com/homepage/homepage.php'); die();}
+header('Location: https://appinfofinal.herogu.garageisep.com/homepage/homepage.php'); die();}
 
 
 
