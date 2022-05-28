8148dee391038fd79ef12ee371e6c93b24a44217
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-05-28 12:09:00 +0200

    push meu deroulant edit pp

--- src/auth/views/landing.php
+++ src/auth/views/landing.php
@@ -59,36 +59,8 @@
   <div id = "container1">
 
 
-  <header>
-            <div id ="logoimg">
-            <a  href="/homepage/homepage.php"><img src="/auth/img/logo_infinite.png" alt="logo"></a>
-            </div>  
-            <nav>
-                <ul class="nav__links">
-                    <li><a href="/homepage/homepage.php">Accueil</a></li>
-                    <li><a href="/<?php echo $chantier ?>" >Votre chantier</a></li>
-                    <li><a href="/forum/forum.php">Forum</a></li>
-                    <li><a href="/faq/faq.php">FAQ</a></li>
-                    <li><a href="/contact/contact_essai.php">Contactez-nous</a></li>
-                    <li><a href="/notre_solution/notre_solution.php">Notre solution</a></li>
-
-                </ul>
-            </nav>
-            <div id="logomemo">
-                <a href="/memory/memory.php"><img src="/memory/img/memoryim.png" alt="memory"></a>
-            </div>
-            <a class="cta" href= "/auth/<?php echo $editprofil?> "> <?php echo $title ?></a>
-
-            <?php
-           
-
-           if(isset($_SESSION['type']) && ($_SESSION['type']) == "Administrateur"){
-            
-            ?>
-            <a class="cta" href= "/admin/admin.php">Admin</a>
-
-            <?php }?>
-        </header>
+  <?php include("header.php");
+  include("menu_deroulant_tel2.php"); ?>
 
     <div id = "title">
              <titre1>Modifier votre profil</titre1>
@@ -241,6 +213,8 @@
          
 
   </div>
+
+  <script src = "../js/index.js"></script>
   </body>
 
   
