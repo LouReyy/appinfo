5ad5da3d997c4d2015becd3360c9299c8b7fcc6d
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-05-31 19:14:46 +0200

    Update DataGestionnaire.php
    ezeez

--- src/Chantier/model/DataGestionnaire.php
+++ src/Chantier/model/DataGestionnaire.php
@@ -54,6 +54,8 @@ $req_card='SELECT DISTINCT Time,Valeur FROM capteur_table WHERE id_utilisateur="
 $resultCard=mysqli_query($conn,$req_card);
 $values_card=mysqli_fetch_all($resultCard, MYSQLI_ASSOC);
 
+if(!empty($values_card)){
+
 $req_son='SELECT DISTINCT Time,Valeur FROM `capteur_table`WHERE type="sonore" AND id_utilisateur="'.$id.'" ORDER BY Time DESC LIMIT 20;';
 $resultSon=mysqli_query($conn,$req_son);
 $values_son=mysqli_fetch_all($resultSon, MYSQLI_ASSOC);
@@ -115,5 +117,7 @@ $lastCard=getLast($values_lastCard);
 $lastSon=getLast($values_lastSon);
 $lastTemp=getLast($values_lastTemp);
 $lastCO2=getLast($values_lastCO2);
+
+}
 
 ?>
