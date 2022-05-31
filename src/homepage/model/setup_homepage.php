823b2aea4b7b48c5168927d96dac1a365501043e
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-05-31 19:48:58 +0200

    Update setup_homepage.php

--- src/homepage/model/setup_homepage.php
+++ src/homepage/model/setup_homepage.php
@@ -16,10 +16,20 @@ if(isset($_SESSION['user'])){
     else{
         $chantier = "VotreChantier/votrechantier.php";
     }
+
+
+
+
+    if(isset($_SESSION['type'])){
     if(($_SESSION['type'] == "Administrateur")){
 
         $chantier = "VotreChantier/votrechantier.php";
     }
+}
+
+
+
+
 
     if(isset($_SESSION['email'])){
 
