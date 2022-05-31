bc95b90f3dfa545e5360033121d80f5a7508ce84
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-05-31 15:33:52 +0200

    a
    za

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -10,6 +10,7 @@ include("model/setup_homepage.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/homepage.css" media="screen" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
     <link rel="icon" type="image/png" href="img/InfiniteMeasures.png" />
 
     <title>Tech4Health</title>
