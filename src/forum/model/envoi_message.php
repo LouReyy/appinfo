5d5217103f25c7ac3a36124d88cd50b1430d2595
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-05-31 15:14:31 +0200

    Update envoi_message.php

--- src/forum/model/envoi_message.php
+++ src/forum/model/envoi_message.php
@@ -16,11 +16,12 @@ session_start();
     $content = htmlspecialchars($_POST['content']);
     $topic = htmlspecialchars($_POST['topic']);
     $pseudo_user = $data['pseudo'];
+    $id_util = $_SESSION['id'];
 
 
    
-    $insert = $bdd->prepare('INSERT INTO message(topic,content,pseudo_user) VALUES(:topic, :content  , :pseudo_user)');
-    $insert->execute(array('topic' =>$topic , 'content' => $content, 'pseudo_user' => $pseudo_user ));
+    $insert = $bdd->prepare('INSERT INTO message(topic,content,pseudo_user,id_util) VALUES(:topic, :content  , :pseudo_user,:id_util)');
+    $insert->execute(array('topic' =>$topic , 'content' => $content, 'pseudo_user' => $pseudo_user,'id_util' => $id_util ));
 
     header('Location: ../forum.php?reg_err=success'); die();
 
