f5aa2741f9ecf72f0eca88ca66ab66b78f195d09
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-05-31 15:28:20 +0200

    za
    ya tout

--- src/cgu/cgu.php
+++ src/cgu/cgu.php
@@ -33,7 +33,8 @@ if(isset($_SESSION['user'])){
 </head>
 <body>
 
-<?php include("views/header.php"); ?>
+<?php include("views/header.php");
+ ?>
 
        
 
@@ -227,44 +228,7 @@ if(isset($_SESSION['user'])){
         </div>
         </div>
 </body>
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class=" footer-col">
-                <img src="infinite.png" class="logo">
-                </div>
-            <div class=" footer-col">
-                <h4>NAVIGATION</h4>
-                <ul>
-                    <li><a href= "#">Accueil</a></li>
-                    <li><a href= "#">Votre chantier</a></li>
-                    <li><a href= "#">Forum</a></li>
-                    <li><a href= "#">Contactez-nous</a></li>
-                </ul>
-            </div>
-            <div class=" footer-col">
-                <h4>PLUS D'INFOS</h4>
-                <ul>
-                    <li><a href= "#">Inscription</a></li>
-                    <li><a href= "#">Connexion</a></li>
-                    <li><a href= "#">Mentions Légales</a></li>
-                    <li><a href= "#">CGU</a></li>
-                </ul>
-            </div>
-            <div class=" footer-col">
-                <h4>SUIVEZ-NOUS</h4>
-                <div class="social-links">
-                    <a href= "#"><i class="fab fa-facebook-f"></i></a>
-                    <a href= "#"><i class="fab fa-twitter"></i></a>
-                    <a href= "#"><i class="fab fa-instagram"></i></a>
-                    <a href= "#"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-                
-            </div>
-        </div>
-    </div>
-    
-</footer>
+<?php include("views/footer.php") ?>
 </html>
     
     
--- src/notre_solution/views/footer.php
+++ src/notre_solution/views/footer.php
@@ -1,26 +1,25 @@
 <footer class="footer">
-    <div class="footerContent">
+    <div class="container">
         <div class="row">
             <div class=" footer-col">
-                <img src="/auth/img/logo_infinite.png"  class="logo">
+                <img src="infinite.png" class="logo">
                 </div>
             <div class=" footer-col">
                 <h4>NAVIGATION</h4>
                 <ul>
-                    <li><a href= "/homepage/homepage.php">Accueil</a></li>
-                    <li><a href= "/<?php echo $chantier ?>">Votre chantier</a></li>
-                    <li><a href= "/forum/forum.php">Forum</a></li>
-                    <li><a href= "/faq/faq.php">FAQ</a></li>
-                    <li><a href= "/contact/contact_essai.php">Contactez-nous</a></li>
-                    <li><a href= "/notre_solution/notre_solution.php">Notre solution</a></li>
+                    <li><a href= "#">Accueil</a></li>
+                    <li><a href= "#">Votre chantier</a></li>
+                    <li><a href= "#">Forum</a></li>
+                    <li><a href= "#">Contactez-nous</a></li>
                 </ul>
             </div>
             <div class=" footer-col">
                 <h4>PLUS D'INFOS</h4>
                 <ul>
-                    <li><a href= "/auth/views/inscription.php">Inscription</a></li>
-                    <li><a href= "/auth/model/connexion.php">Connexion</a></li>
-                    <li><a href= "/cgu/cgu.php">Mentions Légales</a></li>
+                    <li><a href= "#">Inscription</a></li>
+                    <li><a href= "#">Connexion</a></li>
+                    <li><a href= "#">Mentions Légales</a></li>
+                    <li><a href= "#">CGU</a></li>
                 </ul>
             </div>
             <div class=" footer-col">
