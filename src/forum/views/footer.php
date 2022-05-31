0e827dbc0b9eab28c5a8d4ca80843924bc3558ee
Martyna0 <95086387+Martyna0@users.noreply.github.com>
2022-05-31 18:40:28 +0200

    footer forum

--- src/forum/views/footer.php
+++ src/forum/views/footer.php
@@ -2,34 +2,32 @@
     <div class="footerContent">
         <div class="row">
             <div class=" footer-col">
-            <img src="/homepage/img/infinite.png" class="logo">
+                <img src="/homepage/img/InfiniteMeasures.png" class="logo">
                 </div>
             <div class=" footer-col">
                 <h4>NAVIGATION</h4>
                 <ul>
-                    <li><a href= "/homepage/homepage.php">Accueil</a></li>
+                    <li><a href= "/home">Accueil</a></li>
                     <li><a href= "/<?php echo $chantier ?>">Votre chantier</a></li>
                     <li><a href= "/forum/forum.php">Forum</a></li>
-                    <li><a href= "/faq/faq.php">FAQ</a></li>
                     <li><a href= "/contact/contact_essai.php">Contactez-nous</a></li>
-                    <li><a href= "/notre_solution/notre_solution.php">Notre solution</a></li>
                 </ul>
             </div>
             <div class=" footer-col">
                 <h4>PLUS D'INFOS</h4>
                 <ul>
                     <li><a href= "/auth/views/inscription.php">Inscription</a></li>
-                    <li><a href= "/auth/model/connexion.php">Connexion</a></li>
-                    <li><a href= "/cgu/cgu.php">Mentions Légales</a></li>
+                    <li><a href= "/auth/index.php">Connexion</a></li>
+                    <li><a href= "/cgu/cgu.php">Mentions Légales et CGU</a></li>
                 </ul>
             </div>
             <div class=" footer-col">
                 <h4>SUIVEZ-NOUS</h4>
                 <div class="social-links">
-                    <a href= "#"><i class="fab fa-facebook-f"></i></a>
-                    <a href= "#"><i class="fab fa-twitter"></i></a>
-                    <a href= "#"><i class="fab fa-instagram"></i></a>
-                    <a href= "#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href= "https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                    <a href= "https://twitter.com"><i class="fab fa-twitter"></i></a>
+                    <a href= "https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                    <a href= "https://www.linkedin.com"><i class="fab fa-linkedin-in"></i></a>
                 </div>
                 
             </div>
