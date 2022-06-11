fcc4795076b67d128a23f84616c6131de0fc0cc0
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-06-11 17:27:23 +0200

    push test homepage

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -1,5 +1,21 @@
 <?php
 include("model/setup_homepage.php");
+
+echo("test");
+
+$ch = curl_init();
+curl_setopt(
+$ch,
+CURLOPT_URL,
+"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=G9-C");
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$data = curl_exec($ch);
+curl_close($ch);
+echo "Raw Data:<br />";
+echo("$data");
+
+
 ?>
 
 <!DOCTYPE html>
