7d1e88cd096076eba344c59b872e817d9296ea9e
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-06-11 23:15:00 +0200

    trame

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -16,14 +16,22 @@ echo "Raw Data:<br />";
 echo("$data");
 var_dump($data);
 var_dump($ch);
-$response = curl_exec($ch);
 
-if ($response === false) 
-    $response = curl_error($ch);
+$data_tab = str_split($data,33);
+echo "Tabular Data:<br />";
+for($i=0, $size=count($data_tab); $i<$size; $i++){
+echo "Trame $i: $data_tab[$i]<br />";
+}
 
-echo stripslashes($response);
-
-curl_close($ch)
+$trame = $data_tab[1];
+// décodage avec des substring
+$t = substr($trame,0,1);
+$o = substr($trame,1,4);
+// …
+// décodage avec sscanf
+list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
+sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
 
 
 
