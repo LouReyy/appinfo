a5db070da0a6cd5da9a9851d7bf05136f215d3a0
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-06-11 17:19:10 +0200

    push logs paserelle

--- src/Chantier/model/DataChantier.php
+++ src/Chantier/model/DataChantier.php
@@ -130,4 +130,18 @@ $lastCO2=getLast($values_lastCO2);
 
 }
 
+$ch = curl_init();
+curl_setopt(
+$ch,
+CURLOPT_URL,
+"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=G9-C");
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$data = curl_exec($ch);
+curl_close($ch);
+echo "Raw Data:<br />";
+echo("$data");
+
+
+
 ?>
