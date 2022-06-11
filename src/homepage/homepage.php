b98592a3825dfd6def12d0cebf9683abf65d51f8
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-06-11 22:57:00 +0200

    p

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -11,7 +11,6 @@ CURLOPT_URL,
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $data = curl_exec($ch);
-curl_close($ch);
 echo "Raw Data:<br />";
 echo("$data");
 var_dump($data);
