0ef734d0c90e1ccbb35d37b434e8e6b7dcfbe2a7
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-06-11 23:28:15 +0200

    ok

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -7,7 +7,7 @@ $ch = curl_init();
 curl_setopt(
 $ch,
 CURLOPT_URL,
-"http://www.google.com");
+"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=G9-C");
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $data = curl_exec($ch);
