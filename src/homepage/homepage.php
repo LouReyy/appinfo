594205354fbe6f252402a59515d8c76f3d128718
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-06-11 23:16:05 +0200

    push debut trames

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -16,25 +16,6 @@ echo "Raw Data:<br />";
 echo("$data");
 var_dump($data);
 var_dump($ch);
-
-$data_tab = str_split($data,33);
-echo "Tabular Data:<br />";
-for($i=0, $size=count($data_tab); $i<$size; $i++){
-echo "Trame $i: $data_tab[$i]<br />";
-}
-
-$trame = $data_tab[1];
-// décodage avec des substring
-$t = substr($trame,0,1);
-$o = substr($trame,1,4);
-// …
-// décodage avec sscanf
-list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
-sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
-
-
-
 ?>
 
 <!DOCTYPE html>
