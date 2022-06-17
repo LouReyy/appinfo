8a5861e788f1d5885621131c5a137ca866e0e790
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-06-17 23:49:59 +0200

    za
    za

--- src/Chantier/PageChantier.php
+++ src/Chantier/PageChantier.php
@@ -52,7 +52,7 @@
         </header>
         <?php include("views/menu_deroulant_tel.php") ?>
         <div id="sidebar">
-            <button href = ../homepage/homepage.php> Charger les données </button>
+            <a class = btn_data href = ../homepage/homepage.php> Charger les données </a>
             <div class="noms_onglets">
                 <div class="onglets" data-anim="1">
                     <div class="ongletInactif">
