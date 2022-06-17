b6672b7463ced0b730d360c9a88181cb4975d367
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-06-17 18:55:20 +0200

    Update homepage.php

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -21,7 +21,7 @@ $j=intdiv($n,33);
 //essayons de récupérer la première ligne par exemple
 //Maintenant récupérons toutes les lignes de la variable data
 $Lines=array();
-for ($i=200;$i<210;$i++){
+for ($i=0;$i<10;$i++){
     $line=substr($data,33*$i,33);$Lines[$i]=$line;
 }
 //print_r($Lines);Ok ça marche
