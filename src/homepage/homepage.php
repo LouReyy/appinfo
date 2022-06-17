84308cdbc04567b0bff5cc2be88ac59dfc5eebcf
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-06-17 21:49:45 +0200

    Update homepage.php

--- src/homepage/homepage.php
+++ src/homepage/homepage.php
@@ -64,7 +64,7 @@ for ($i=5000;$i<10000;$i++){
 
         //On insère dans la base de donnée
 
-        $req= $bdd->prepare('INSERT INTO `capteur_table`(`time`, `valeur`, `type`, `id_utilisateur`, `id_chantier`) VALUES (:time, :valeur, :type, :id_utilisateur, :id_chantier) WHERE NOT EXISTS (SELECT * FROM TEST WHERE time = '.$time[$i].'');
+        $req= $bdd->prepare('INSERT INTO `capteur_table`(`time`, `valeur`, `type`, `id_utilisateur`, `id_chantier`) VALUES (:time, :valeur, :type, :id_utilisateur, :id_chantier) WHERE NOT EXISTS (SELECT * FROM TEST WHERE Time = '.$time[$i].'');
         ');
         $req->execute(array(
             'time' => $newdate,
