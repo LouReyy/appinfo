bfaa4a8e84a6c6801e9f6278c86780c148711525
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-06-18 12:43:18 +0200

    Update DataChantier.php

--- src/Chantier/model/DataChantier.php
+++ src/Chantier/model/DataChantier.php
@@ -102,9 +102,6 @@ $Xtemp=array_reverse($Xtemp);
 $Ytemp=tableY($values_temp);
 $Ytemp=array_reverse($Ytemp);
 
-var_dump($Xtemp);
-var_dump($Ytemp);
-
 $Xcard=tableX($values_card); $Ycard=tableY($values_card);
 $Xcard=array_reverse($Xcard); $Ycard=array_reverse($Ycard);
 
