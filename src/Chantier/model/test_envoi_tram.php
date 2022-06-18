bbae2678928ada33ab6a0d91172d47c2f4f89ba1
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-06-18 16:04:45 +0200

    Update test_envoi_tram.php

--- src/Chantier/model/test_envoi_tram.php
+++ src/Chantier/model/test_envoi_tram.php
@@ -1,7 +1,10 @@
 <?php
 
 
-header('Location : "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=G9-C"')
+header('Location : http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=G9-C');
+
+die();
+
 
 
 
