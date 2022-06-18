c95fe79d08a7ec9ef51fafc742bee21dce234d4d
LouKingFr <81685145+LouKingFr@users.noreply.github.com>
2022-06-18 12:04:37 +0200

    Update add_data.php

--- src/Chantier/model/add_data.php
+++ src/Chantier/model/add_data.php
@@ -23,7 +23,7 @@ $time=array();
 //essayons de récupérer la première ligne par exemple
 //Maintenant récupérons toutes les lignes de la variable data
 $Lines=array();
-for ($i=15000;$i<$n;$i++){
+for ($i=5000;$i<$j;$i++){
     $line=substr($data,33*$i,33);
     $Lines[$i]=$line;
 }
@@ -33,7 +33,7 @@ for ($i=15000;$i<$n;$i++){
 echo(count($Lines));
 
 
-for ($i=15000;$i<$n;$i++){
+for ($i=5000;$i<$j;$i++){
     $val[$i]=substr($Lines[$i],9,4);
     $time[$i]=substr($Lines[$i],19,14);
     $type[$i] =substr($Lines[$i],6,1);
