077f01e0f505d7d05cf4edb29aab399e1713f746
LouKingFr <99537090+LouReyy@users.noreply.github.com>
2022-06-21 09:19:02 +0200

    PUSH TRAM

--- src/Chantier/model/test_envoi_tram.php
+++ src/Chantier/model/test_envoi_tram.php
@@ -1,7 +1,7 @@
 <?php
 
 
-header('Location: http://projets-tomcat.isep.fr:8080/appService/?ACTION=GETLOG&TEAM=G9-C&TRAM=G9-C15012570000074202206171520131');
+header('Location: http://projets-tomcat.isep.fr:8080/appService/?TRAM=G9-C15012570000074202206171520131');
 
 
 
